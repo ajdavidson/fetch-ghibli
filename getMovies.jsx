229e9904b1b6896f342136f566d3c89873b27f93
AJDavidson <89638345+ajdavidson@users.noreply.github.com>
2021-11-11 15:23:31 -0700

    code cleanup

--- getMovies.jsx
+++ getMovies.jsx
@@ -14,7 +14,6 @@ function App() {
 
   const handleShow = (title, banner) => {
     setShow(true);
-    //console.log('Banner Props:' + title);
 
     setTitle(title);
     setBanner(banner);
@@ -74,25 +73,17 @@ function App() {
                       <li class="list-group-item"><i class="fas fa-clock"></i> {movie.running_time} min</li>
                       <li class="list-group-item"><i class="fas fa-calendar-alt"></i> {movie.release_date}</li>
                       <li class="list-group-item"><img src={movie.rt_score<70 ? 'rt-sp.png': movie.rt_score<90 ? 'rt-f.png': 'rt-cf.png'} width="25px" /> {movie.rt_score}%</li>
-                      {/*<li class="list-group-item"> <a href={movie.movie_banner} target="_blank" class="btn btn-outline-secondary">*/}
-                      {/*  <i className="fas fa-image fa-lg"></i> Movie Banner <i className="fas fa-external-link-alt fa-xs"></i></a> </li>*/}
                       <li className="list-group-item">
                         <Button variant="outline-secondary" x={movie.title}
                            onClick={()=>{
                            handleShow(movie.title,movie.movie_banner);
                            //alert('yep');
                            }
-
                          }><i className="fas fa-image fa-lg"></i> Movie Banner <i className="fas fa-external-link-alt fa-xs"></i>
                       </Button></li>
                     </ul>
 
-                    <Modal show={show} size="lg" onHide={handleClose}>
-                      <Modal.Header closeButton>
-                        <Modal.Title><i className="fas fa-film"></i> {title}</Modal.Title>
-                      </Modal.Header>
-                      <Modal.Body><img src={banner} width="100%px"/></Modal.Body>
-                    </Modal>
+
                   </div>
                   <img class="card-img-left" src={movie.image} alt="Card image cap" height="500px" />
                 </div>
@@ -104,7 +95,12 @@ function App() {
 
       ))
       }
-
+      <Modal show={show} size="lg" onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title><i className="fas fa-film"></i> {title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body><img src={banner} width="100%px"/></Modal.Body>
+      </Modal>
     </Container >
   );
 }
