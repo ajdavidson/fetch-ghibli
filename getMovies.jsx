1b31042dc7085711f1c130ff2d383f7ec3b1900e
AJDavidson <89638345+ajdavidson@users.noreply.github.com>
2021-11-12 16:31:20 -0700

    finalise grid and icon tweeks

--- getMovies.jsx
+++ getMovies.jsx
@@ -1,9 +1,10 @@
 function App() {
   const { useState, useEffect } = React;
-  const { Container, Row, Col, Button, Modal, Dropdown, Image, Card, ListGroup, ListGroupItem, Fade, Spinner } = ReactBootstrap;
+  const { Container, Row, Col, Button, Modal, Dropdown, Image, Card, ListGroup, ListGroupItem, Fade, Spinner, Transition } = ReactBootstrap;
   const [data, setData] = useState([]);
   const [url, setUrl] = useState("https://ghibliapi.herokuapp.com/films/");
-  const [fetching, setFetching] = useState('false');
+  const [fetching, setFetching] = useState('grow');
+  const [open, setOpen] = useState(true);
 
   //const [query, setQuery] = useState("");
   //const [rtImg, setRTImg] = useState('rt-cf.png');
@@ -42,18 +43,16 @@ function App() {
   return (
     <Container>
 
+      <Button variant="outline-secondary" href="#top" id="toTop"><i className="fas fa-angle-double-up"/></Button>
 
-      <Button variant="outline-secondary" href="#top" id="toTop"><i
-        className="fas fa-angle-double-up"></i></Button>
-
-      <Fade in={true} appear={true} timeout={3000}>
       <Image src="./Studio_Ghibli_logo.png" width="33%" />
-      </Fade>
-        <Spinner animation={fetching} role="status"/>
+
+      <Spinner animation={fetching} role="status"/>
+
       <p> </p>
       <Dropdown drop="end">
         <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-          <i className="fas fa-film fa-2x"></i>
+          <i className="fas fa-film fa-2x"/>
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
@@ -70,43 +69,50 @@ function App() {
           <div id={movie.id} />
           <Card>
               <Card.Header>
-                  <Card.Title><i class="fas fa-film"></i> {movie.title}</Card.Title>
+                  <Card.Title><i className="fas fa-film"/> {movie.title}</Card.Title>
                   <Card.Subtitle className="mb-2 text-muted">{movie.original_title} ({movie.original_title_romanised})</Card.Subtitle>
               </Card.Header>
             <Row>
-              <Col sm={12} lg={8}>
+              <Col sm={12} lg={6}>
                 <Card.Body>
                   {movie.description}
                 </Card.Body>
                 <ListGroup className="list-group-flush">
-                  <ListGroupItem><i class="fas fa-video"></i> Directed by {movie.director}</ListGroupItem>
-                  <ListGroupItem><i class="fas fa-ticket-alt"></i> {movie.producer} Production</ListGroupItem>
-                  <ListGroupItem><i class="fas fa-clock"></i> {movie.running_time} min</ListGroupItem>
-                  <ListGroupItem><i class="fas fa-calendar-alt"></i> {movie.release_date}</ListGroupItem>
-                  <ListGroupItem><img src={movie.rt_score < 70 ? 'rt-sp.png' : movie.rt_score < 90 ? 'rt-f.png' : 'rt-cf.png'} width="25px" /> {movie.rt_score}%</ListGroupItem>
+                  <ListGroupItem><i className="fas fa-video"/> Directed by {movie.director}</ListGroupItem>
+                  <ListGroupItem><i className="fas fa-ticket-alt"/> {movie.producer} Production</ListGroupItem>
+                  <ListGroupItem><i className="fas fa-clock"/> {movie.running_time} min</ListGroupItem>
+                  <ListGroupItem><i className="fas fa-calendar-alt"/> {movie.release_date}</ListGroupItem>
+                  <ListGroupItem>
+                    {/*<img src={movie.rt_score < 70 ? 'rt-sp.png' : movie.rt_score < 90 ? 'rt-f.png' : 'rt-cf.png'} width="25px" />*/}
+                    {/* {' '+movie.rt_score}%*/}
+                    <Image
+                        src={movie.rt_score < 70 ? 'rt-sp.png' : movie.rt_score < 90 ? 'rt-f.png' : 'rt-cf.png'}
+                        width="25px"
+                    /> {' '+movie.rt_score}%
+                  </ListGroupItem>
                   <ListGroupItem><Button variant="outline-secondary" x={movie.title}
                     onClick={() => {
                       handleShow(movie.title, movie.movie_banner);
                       //alert('yep');
                     }
-                    }><i className="fas fa-image fa-lg"></i> Movie Banner <i className="fas fa-external-link-alt fa-xs"></i>
+                    }><i className="fas fa-image fa-lg"/> Movie Banner <i className="fas fa-external-link-square-alt fa-xs"/>
                   </Button></ListGroupItem>
                 </ListGroup>
               </Col>
-              <Col sm={12} lg={4}><Card.Img  variant="top" src={movie.image} fluid/></Col>
+              <Col sm={12} lg={{span: 5, offset: 1}}><Card.Img src={movie.image} fluid="true"/></Col>
             </Row>
-              <Card.Footer></Card.Footer>
+              <Card.Footer/>
           </Card>
-          < p > </p>
+          <p> </p>
         </React.Fragment >
 
       ))
       }
       <Modal show={show} size="lg" onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title><i className="fas fa-film"></i> {title}</Modal.Title>
+          <Modal.Title><i className="fas fa-film"/> {title}</Modal.Title>
         </Modal.Header>
-        <Modal.Body><img src={banner} width="100%px" /></Modal.Body>
+        <Modal.Body><Card.Img src={banner}/></Modal.Body>
       </Modal>
     </Container >
   );
