eb5f1b5f061ae2e4c921cfb833da416feef29631
AJDavidson <89638345+ajdavidson@users.noreply.github.com>
2021-11-12 11:48:34 -0700

    fixup modal style

--- getMovies.jsx
+++ getMovies.jsx
@@ -1,6 +1,6 @@
 function App() {
   const { useState, useEffect } = React;
-  const { Container, Row, Col, Button, Modal, Dropdown, Image, Card, ListGroup, ListGroupItem } = ReactBootstrap;
+  const { Container, Row, Col, Button, Modal, Dropdown, Image, Card, ListGroup, ListGroupItem, Fade } = ReactBootstrap;
   const [data, setData] = useState([]);
   const [url, setUrl] = useState("https://ghibliapi.herokuapp.com/films/");
   //const [query, setQuery] = useState("");
@@ -39,7 +39,10 @@ function App() {
 
       <Button variant="outline-secondary" href="#top" id="toTop"><i
         className="fas fa-angle-double-up"></i></Button>
+
+      <Fade in={true} appear={true} timeout={3000}>
       <Image src="./Studio_Ghibli_logo.png" width="33%" />
+      </Fade>
 
       <p> </p>
       <Dropdown drop="end">
@@ -60,15 +63,19 @@ function App() {
           {/* target  */}
           <div id={movie.id} />
           <Card>
-            <Row>
-
-              <Col sm={4} md={6}>
-                <Card.Body>
+              <Card.Header>
                   <Card.Title><i class="fas fa-film"></i> {movie.title}</Card.Title>
                   <Card.Subtitle className="mb-2 text-muted">{movie.original_title} ({movie.original_title_romanised})</Card.Subtitle>
-                  <Card.Text>
-                  </Card.Text>
-                </Card.Body>
+              </Card.Header>
+            <Row>
+
+              <Col sm={4} md={6} lg={8}>
+                {/*<Card.Body>*/}
+                {/*  /!*<Card.Title><i class="fas fa-film"></i> {movie.title}</Card.Title>*!/*/}
+                {/*  /!*<Card.Subtitle className="mb-2 text-muted">{movie.original_title} ({movie.original_title_romanised})</Card.Subtitle>*!/*/}
+                {/*  /!*<Card.Text>*!/*/}
+                {/*  /!*</Card.Text>*!/*/}
+                {/*</Card.Body>*/}
                 <Card.Body>
                   {movie.description}
                 </Card.Body>
@@ -88,40 +95,10 @@ function App() {
                   </Button></ListGroupItem>
                 </ListGroup>
               </Col>
-              <Col xs={4} md="6" fluid><Card.Img  variant="top" src={movie.image} fluid /></Col>
+              <Col xs={2} md={4} lg={4}><Card.Img  variant="top" src={movie.image} /></Col>
             </Row>
+              <Card.Footer></Card.Footer>
           </Card>
-          {/* <div class="row">
-            <div class="col">
-              <div class="card">
-                <div class="row card-body">
-                  <div class="col">
-                    <div style={{ padding: "5px", backgroundColor: "#a6a6a6" }} class="text-white">
-                      <h5 class="card-title"><i class="fas fa-film"></i> {movie.title}</h5>
-                      <h6 class="card-subtitle mb-2 text-dark"> {movie.original_title} ({movie.original_title_romanised})</h6>
-                    </div>
-                    <p class="card-text">{movie.description}</p>
-                    <ul class="list-group list-group-flush" style={{ listStyleType: "none" }} key={movie.id}>
-                      <li class="list-group-item"><i class="fas fa-video"></i> Directed by {movie.director}</li>
-                      <li class="list-group-item"><i class="fas fa-ticket-alt"></i> {movie.producer} Production</li>
-                      <li class="list-group-item"><i class="fas fa-clock"></i> {movie.running_time} min</li>
-                      <li class="list-group-item"><i class="fas fa-calendar-alt"></i> {movie.release_date}</li>
-                      <li class="list-group-item"><img src={movie.rt_score < 70 ? 'rt-sp.png' : movie.rt_score < 90 ? 'rt-f.png' : 'rt-cf.png'} width="25px" /> {movie.rt_score}%</li>
-                      <li className="list-group-item">
-                        <Button variant="outline-secondary" x={movie.title}
-                          onClick={() => {
-                            handleShow(movie.title, movie.movie_banner);
-                            //alert('yep');
-                          }
-                          }><i className="fas fa-image fa-lg"></i> Movie Banner <i className="fas fa-external-link-alt fa-xs"></i>
-                        </Button></li>
-                    </ul>
-                  </div>
-                  <img class="card-img-left" src={movie.image} alt="Card image cap" height="500px" />
-                </div>
-              </div>
-            </div>
-          </div> */}
           < p > </p>
         </React.Fragment >
 
